c6fa6c575137cef86b31207cb120ffecac80ded4
Frank Liu <frank.j.liu@berkeley.edu>
2022-11-14 14:14:35 -0800

    Removed Sign_up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Friends from "./components/Friends";
 import Home from "./components/Home";
 import Log_in from "./components/Log_in";
 import Playlist from "./components/Player";
-import Sign_up from "./components/Sign_up";
 import "./App.css";
 
 function App() {
