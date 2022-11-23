b38e8a65b8afa24cfc0241a3ac33bd2ea81a86d7
Frank Liu <frank.j.liu@berkeley.edu>
2022-11-23 01:25:52 -0800

    Added Hover effect to Card

--- frontend/src/components/Image.tsx
+++ frontend/src/components/Image.tsx
@@ -11,14 +11,19 @@ import {
 
 function Image({ name }: { name: string }) {
   const ref = useRef(null);
-  const [button, setButton] = useState("0");
+  const [button, setButton] = useState("opacity-0");
+  const [bgOpacity, setBgOpacity] = useState("hover:opacity-80");
 
   function playButton() {
-    setButton("100");
+    setButton("opacity-100");
   }
 
   function stopButton() {
-    setButton("0");
+    setButton("opacity-0");
+  }
+
+  function playBgOpacity() {
+    setBgOpacity("hover:opacity-80");
   }
 
   return (
@@ -26,12 +31,13 @@ function Image({ name }: { name: string }) {
       <h1 className="text-pink sm:left-[57%] sm:visible sm:text-6xl font-bold tracking-tight leading-tight absolute z-40">{`@${name}`}</h1>
       <div
         ref={ref}
-        className="w-[400px] h-[500px] relative m-5 overflow-hidden"
+        className={`${bgOpacity} w-[400px] h-[500px] relative m-5 overflow-hidden`}
         onMouseOver={playButton}
         onMouseOut={stopButton}
       >
         <div
-          className={`opacity-${button} hover:scale-105 transition duration-[350ms] left-[73%] bottom-[5%] sm:left-[73%] sm:bottom-[5%] absolute bg-pink rounded-full w-20 h-20 z-40`}
+          className={`transition duration-[350ms] ${button} hover:scale-105 left-[73%] bottom-[5%] sm:left-[73%] sm:bottom-[5%] absolute bg-pink rounded-full w-20 h-20 z-40`}
+          onMouseOver={playBgOpacity}
         >
           <FontAwesomeIcon
             className="text-black text-[2.5rem] z-50 absolute left-[35%] bottom-[25%]"
