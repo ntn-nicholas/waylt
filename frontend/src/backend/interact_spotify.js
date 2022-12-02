3680b28ba2b5099115173c5876cf24a62d7f2c32
Octavio Lomeli-Castro <octavio@Octavios-MacBook-Air.local>
2022-12-02 09:15:00 -0800

    clean up

--- frontend/src/backend/interact_spotify.js
+++
@@ -1,7 +0,0 @@
-import { getTokenFromUrl } from './login';
-import SpotifyWebApi from 'spotify-web-api-js';
-
-const spotify = new SpotifyWebApi();
-
-console.log(getTokenFromUrl());
-console.log("hello there");
